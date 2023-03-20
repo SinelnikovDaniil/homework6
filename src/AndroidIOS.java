016805eebe1e074a9de465711562cf714d70be72
Даниил <mackarosha@mail.ru>
2023-03-20 12:13:00 +1000

    homework6 done

--- src/AndroidIOS.java
+++ src/AndroidIOS.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class AndroidIOS {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Какая у вас мобильная операционная система? \nIOS - 0, Android - 1");
+        int clientOS = input.nextInt();
+        System.out.println("В каком году была выпущена ваша модель телефона?");
+        int clientDeviceYear = input.nextInt();
 
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке:");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке:");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке:");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке:");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class LeapYear {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Введите год:");
+        int year = input.nextInt();
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " - високосный год");
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
     }
 }
